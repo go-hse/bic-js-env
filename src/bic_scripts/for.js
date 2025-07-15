47c1d0636a58b26cc10e90f340962ffe2d342c5d
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-07-15 14:17:51 +0200

    inital versio of sandboxed environment

---
+++ src/bic_scripts/for.js
@@ -0,0 +1,20 @@
+let x = 0;
+for (let i = 0; i < 5; i++) {
+    x += i;
+    console.log('x is', x);
+}
+
+let test_name = "test";
+try {
+    test_name = name;
+} catch (ex) {
+    console.log(ex.message);
+}
+
+const output = {
+    x,
+    d: new Date().toString(),
+    test_name
+}
+
+output;

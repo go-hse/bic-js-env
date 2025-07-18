d1c89434d04289fa6a1c1965cab80bc9f45455c2
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2025-07-18 09:20:19 +0200

    added webpack

---
+++ src/html/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+
+const fs = require('fs');
+
+const distPath = path.resolve(__dirname, "..", "..", 'dist');
+console.log("Export to", distPath);
+
+module.exports = {
+    entry: './js/client.mjs', // Haupt-JavaScript-Datei
+    output: {
+        filename: 'bundle.js',
+        path: distPath,
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'], // CSS wird inline ins JS eingebettet
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader', // optional für HTML-Include
+                options: {
+                    sources: false,
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body', // Bundle wird automatisch eingebettet
+        }),
+        new HtmlInlineScriptPlugin()
+    ],
+    mode: 'production',
+};
